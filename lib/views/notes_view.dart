d3ed5f345c3c390c9899112d6d7a38b56c048384
Amaar Abd <abdamaar64@gmail.com>
2025-10-29 23:26:52 +0300

    show Moda lBottom Sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/add_note_botoom_sheet.dart';
 import 'package:notes_app/widgets/body_note.dart';
 
 class NotesView extends StatelessWidget {
@@ -8,7 +9,14 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return const AddNoteBottomSheet();
+            },
+          );
+        },
         child: Icon(Icons.add, color: Colors.black),
         backgroundColor: Colors.cyan,
       ),
@@ -16,3 +24,5 @@ class NotesView extends StatelessWidget {
     );
   }
 }
+
+
