c3ed5dcdb866803f4a2167a03b2da363f76b7ca4
Amaar Abd <abdamaar64@gmail.com>
2025-11-01 17:35:06 +0300

    create add note cubit , add note state

--- lib/widgets/add_note_botoom_sheet.dart
+++ lib/widgets/add_note_botoom_sheet.dart
@@ -10,17 +10,64 @@ class AddNoteBottomSheet extends StatelessWidget {
     return Container(
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              CustomTextField(hintText: 'Title'),
-              SizedBox(height: 30),
-              CustomTextField(hintText: 'discreption', maxLines: 5),
-              SizedBox(height: 50),
-              CustomBottom(),
-            ],
+        child: SingleChildScrollView(child: FormAddNoteBottomShet()),
+      ),
+    );
+  }
+}
+
+class FormAddNoteBottomShet extends StatefulWidget {
+ const FormAddNoteBottomShet({super.key});
+
+  @override
+  State<FormAddNoteBottomShet> createState() => _FormAddNoteBottomShetState();
+}
+
+class _FormAddNoteBottomShetState extends State<FormAddNoteBottomShet> {
+  GlobalKey<FormState> formkey = GlobalKey();
+
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  String? title, subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formkey,
+      autovalidateMode: autovalidateMode,
+
+      child: Column(
+        children: [
+          CustomTextFormField(
+            hintText: 'Title',
+            onSaved: (value) {
+              title = value;
+            },
           ),
-        ),
+          SizedBox(height: 30),
+          CustomTextFormField(
+            hintText: 'discreption',
+            maxLines: 5,
+            onSaved: (value) {
+              subtitle = value;
+            },
+          ),
+          SizedBox(height: 50),
+
+          CustomBottom(
+            ontap: () {
+              if (formkey.currentState!.validate()) {
+                // for turn play check manual
+                formkey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {
+                  
+                });
+              }
+            },
+          ),
+        ],
       ),
     );
   }
